d3cf930c3a2af86b7702002187bdcaedebfbdf49
yashmish18 <yashmish10930@gmail.com>
2026-04-17 04:29:29 +0530

    update: push to new branch

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        workerThreads: false,
+        cpus: 1,
+    },
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
+    },
+    outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
